22a88f90dbbb057bd7d3a5c647640cb38271118c
TheoLeanse <theoleanse@gmail.com>
2015-06-09 12:20:57 +0100

    returns correct values for multiples of 3, 5, and both

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,10 +1,14 @@
 def fizzbuzz(number)
   return "please enter a positive integer" if number < 1
-  if number == 3
+  if number % 3 == 0 and number % 5 == 0
+    "fizzbuzz"
+  elsif number == 3
     'fizz'
   elsif number % 3 == 0
     "fizz"
   elsif number == 5
+    "buzz"
+  elsif number % 5 == 0
     "buzz"
   else
     number
