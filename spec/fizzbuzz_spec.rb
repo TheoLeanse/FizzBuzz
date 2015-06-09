d039c02e4a32538d040f6ca81f75e2f8c384ea28
TheoLeanse <theoleanse@gmail.com>
2015-06-09 13:01:26 +0100

    Rspec file now tests for fizzbuzz as a module

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -2,24 +2,24 @@ require 'fizzbuzz'
 
 describe 'fizzbuzz' do
   it 'returns "fizz" when passed 3' do
-    expect(fizzbuzz(3)).to eq 'fizz'
+    expect(3.fizzbuzz).to eq 'fizz'
   end
   it 'returns "buzz" when passed 5' do
-    expect(fizzbuzz(5)).to eq "buzz"
+    expect(5.fizzbuzz).to eq "buzz"
   end
   it 'returns number when passed number other than 3 or 5' do
-    expect(fizzbuzz(1)).to eq 1
+    expect(1.fizzbuzz).to eq 1
   end
   it "returns \"fizz\" when passed a multiple of 3" do
-    expect(fizzbuzz(6)).to eq "fizz"
+    expect(6.fizzbuzz).to eq "fizz"
   end
   it "returns \"please enter a positive integer\" when passed number below 1" do
-    expect(fizzbuzz(-1)).to eq "please enter a positive integer"
+    expect(-1.fizzbuzz).to eq "please enter a positive integer"
   end
   it "returns \"buzz\" when passed a multiple of 5" do
-    expect(fizzbuzz(10)).to eq "buzz"
+    expect(10.fizzbuzz).to eq "buzz"
   end
   it "returns \"fizzbuzz\" when passed a multiple of both 3 and 5" do
-    expect(fizzbuzz(15)).to eq "fizzbuzz"
+    expect(15.fizzbuzz).to eq "fizzbuzz"
   end
 end
