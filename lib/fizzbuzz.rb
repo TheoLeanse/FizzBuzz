adb5dfe2f0e57ec48812ab9e369f0b6310eb30a9
TheoLeanse <theoleanse@gmail.com>
2015-06-09 12:14:21 +0100

    tests for integers lower than 1

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,9 +1,12 @@
 def fizzbuzz(number)
+  return "please enter a positive integer" if number < 1
   if number == 3
     'fizz'
+  elsif number % 3 == 0
+    "fizz"
   elsif number == 5
     "buzz"
   else
-    number  
+    number
   end
 end
