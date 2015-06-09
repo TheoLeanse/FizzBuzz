3b67e8e79983f88ce9b41eba5d74a0c3f3f13c56
TheoLeanse <theoleanse@gmail.com>
2015-06-09 12:14:44 +0100

    adds an example for integers lower than 1

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -8,6 +8,12 @@ describe 'fizzbuzz' do
     expect(fizzbuzz(5)).to eq "buzz"
   end
   it 'returns number when passed number other than 3 or 5' do
-    expect(fizzbuzz(0)).to eq 0
+    expect(fizzbuzz(1)).to eq 1
+  end
+  it "returns \"fizz\" when passed a multiple 3" do
+    expect(fizzbuzz(6)).to eq "fizz"
+  end
+  it "returns \"please enter a positive integer\" when passed number below 1" do
+    expect(fizzbuzz(-1)).to eq "please enter a positive integer"
   end
 end
