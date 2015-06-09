a3b18ae9e3652e4c8705c77169f51fe0af930e1b
TheoLeanse <theoleanse@gmail.com>
2015-06-09 12:35:05 +0100

    refactors fizzbuzz.rb

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,13 +1,13 @@
+# def divisible_by(number, divisor)
+#   number % divisor == 0
+# end
+
 def fizzbuzz(number)
   return "please enter a positive integer" if number < 1
   if number % 3 == 0 and number % 5 == 0
     "fizzbuzz"
-  elsif number == 3
-    'fizz'
   elsif number % 3 == 0
     "fizz"
-  elsif number == 5
-    "buzz"
   elsif number % 5 == 0
     "buzz"
   else
