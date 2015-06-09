a976d3b6838e81e3407902a74ab5d0f66be16a59
TheoLeanse <theoleanse@gmail.com>
2015-06-09 12:01:23 +0100

    adds test to fizzbuzz_rspec.rb

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -4,4 +4,10 @@ describe 'fizzbuzz' do
   it 'returns "fizz" when passed 3' do
     expect(fizzbuzz(3)).to eq 'fizz'
   end
+  it 'returns "buzz" when passed 5' do
+    expect(fizzbuzz(5)).to eq "buzz"
+  end
+  it 'returns number when passed number other than 3 or 5' do
+    expect(fizzbuzz(0)).to eq 0
+  end
 end
