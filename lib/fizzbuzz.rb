bb8d8f3eef5197af63a9629bad6c843ad26c2ee1
TheoLeanse <theoleanse@gmail.com>
2015-06-09 12:59:23 +0100

    Adds method to module 'Fizzbuzz', and includes module in Class

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,16 +1,23 @@
 # def divisible_by(number, divisor)
 #   number % divisor == 0
 # end
+module Fizzbuzz
 
-def fizzbuzz(number)
-  return "please enter a positive integer" if number < 1
-  if number % 3 == 0 and number % 5 == 0
-    "fizzbuzz"
-  elsif number % 3 == 0
-    "fizz"
-  elsif number % 5 == 0
-    "buzz"
-  else
-    number
+  def fizzbuzz
+    return "please enter a positive integer" if self < 1
+    if self % 3 == 0 and self % 5 == 0
+      "fizzbuzz"
+    elsif self % 3 == 0
+      "fizz"
+    elsif self % 5 == 0
+      "buzz"
+    else
+      self
+    end
   end
+
+end
+
+class Integer
+  include Fizzbuzz
 end
