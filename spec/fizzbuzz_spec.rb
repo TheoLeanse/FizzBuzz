69a6b1c71ba2c90fb834454436ded61a4dd7c1fc
TheoLeanse <theoleanse@gmail.com>
2015-06-09 12:20:23 +0100

    tests for multiples of 3, 5, and both

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -10,10 +10,16 @@ describe 'fizzbuzz' do
   it 'returns number when passed number other than 3 or 5' do
     expect(fizzbuzz(1)).to eq 1
   end
-  it "returns \"fizz\" when passed a multiple 3" do
+  it "returns \"fizz\" when passed a multiple of 3" do
     expect(fizzbuzz(6)).to eq "fizz"
   end
   it "returns \"please enter a positive integer\" when passed number below 1" do
     expect(fizzbuzz(-1)).to eq "please enter a positive integer"
+  end
+  it "returns \"buzz\" when passed a multiple of 5" do
+    expect(fizzbuzz(10)).to eq "buzz"
+  end
+  it "returns \"fizzbuzz\" when passed a multiple of both 3 and 5" do
+    expect(fizzbuzz(15)).to eq "fizzbuzz"
   end
 end
